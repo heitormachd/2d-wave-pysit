2e9b75c5fb697e38b546001e328bd7a79248dab4
heitormachd <heitormachd@gmail.com>
2023-11-09 17:29:47 -0300

    def gaussian

--- AcousticWave2D.py
+++ AcousticWave2D.py
@@ -2,29 +2,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.ndimage import laplace
 from PIL import Image
+import cv2
 
-# def create_gif_and_video(time):
-#     images_for_gif = []
-#     images_for_video = []
-#     for t in range(time):
-#         images_for_gif.append(Image.open(f"images/plot_{t}.png"))
-#
-#         img = cv2.imread(f"images/plot_{t}.png")
-#         height, width, layers = img.shape
-#         size = (width, height)
-#         images_for_video.append(img)
-#
-#     out = cv2.VideoWriter(
-#         'project.avi', cv2.VideoWriter_fourcc(*'DIVX'), 30, size)
-#
-#     for i in range(len(images_for_video)):
-#         out.write(images_for_video[i])
-#     out.release()
-#
-#     images_for_gif[0].save('simulation.gif', format='GIF',
-#                            append_images=images_for_gif[1:],
-#                            save_all=True,
-#                            duration=50, loop=0)
+
+def create_gif_and_video(time):
+    images_for_gif = []
+    images_for_video = []
+    for t in range(time):
+        images_for_gif.append(Image.open(f"images/plot_{t}.png"))
+
+        img = cv2.imread(f"images/plot_{t}.png")
+        height, width, layers = img.shape
+        size = (width, height)
+        images_for_video.append(img)
+
+    out = cv2.VideoWriter(
+        'project.avi', cv2.VideoWriter_fourcc(*'DIVX'), 30, size)
+
+    for i in range(len(images_for_video)):
+        out.write(images_for_video[i])
+    out.release()
+
+    images_for_gif[0].save('simulation.gif', format='GIF',
+                           append_images=images_for_gif[1:],
+                           save_all=True,
+                           duration=50, loop=0)
 
 
 T = 3  # [s]
@@ -39,89 +41,74 @@ zmax = 0.8
 
 p = 1
 
-Lx = (xmax - xmin) * p
-Lz = (zmax - zmin) * p  # [m]
+Lx = (xmax - xmin)
+Lz = (zmax - zmin)  # [m]
 nx = 91 * p
 nz = 71 * p  # [n]
 dx = (Lx/nx)
 dz = (Lz/nz)  # [m/n]
-
-
-Tt = np.linspace(0, T-dt, nt)
-
 
 xrow = np.linspace(xmin, xmax, nx)
 zrow = np.linspace(zmin, zmax, nz)
 tup = np.meshgrid(zrow, xrow)
 grid = tuple([(x.reshape(nx*nz)) for x in tup])
 
-
 Z = grid[0]
 drop_threshold = 1e-7
 
 
-def _gaussian_derivative_pulse(ZZ, threshold, **kwargs):
+def _gaussian_derivative_pulse(ZZ, threshold):
     """ Derivative of a Gaussian at a specific sigma """
-    T = -100.0*ZZ*np.exp(-(ZZ**2)/1e-4)
-    T[np.where(abs(T) < threshold)] = 0
-    return T
+    P = -100.0 * ZZ * np.exp(-(ZZ ** 2) / 1e-4)
+    P[np.where(abs(T) < threshold)] = 0
+    return P
 
 
-# depth = domain.z.lbound + d * domain.z.length
-d1 = zmin + 0.45 * (zmax - zmin)
-d2 = zmin + 0.65 * (zmax - zmin)
-# d = 0,45 e 0,65 (1º e 2º loops respectivamente)
+# Usado para criar os refletores:
+dC = np.zeros(nx*nz)
+for d in (0.45, 0.65):
+    # depth is a percentage of the length
+    depth = zmin + d * Lz
+    dC += _gaussian_derivative_pulse(Z - depth, drop_threshold)
 
-gaussian_derivative1 = _gaussian_derivative_pulse(Z - d1, drop_threshold)
-gaussian_derivative2 = _gaussian_derivative_pulse(Z - d2, drop_threshold)
+dC = dC.reshape(nx, nz)
+dC = dC.T
+plt.plot(dC)
+plt.show()
 
-dc = gaussian_derivative1 + gaussian_derivative2
-dC = dc.reshape(6461, 1)
-def ricker(t, a):
-    # x = np.diff(np.diff(np.exp(-t**2/a))/dt)/dt
-    x = np.exp(-t**2/a)*(4*t**2/a**2-2/a)
+
+
+def gaussian(t, a, style):
+    if style == 'ricker':
+        x = np.exp(-t**2/a)*(4*t**2/a**2-2/a)
+    elif style == 'gaussian_1stderivative':
+        x= np.exp(-t**2/a)*(-2*t/a)
     return x/np.max(np.abs(x))
 
 
-tt = Tt-T/21
-am = 1e-3
-source = ricker(tt, am)
-
-# plt.figure()
-# plt.plot(source)
-# plt.show()
+# atraso do pulso
+Tt = np.linspace(0, T-dt, nt)
+tt = Tt - T/21
+am = 1e-3  # amplitude
+#source_style = 'ricker'
+source_style = 'gaussian_1stderivative'
+source = gaussian(tt, am, source_style)
+plt.figure()
+plt.plot(source)
+plt.show()
 
 p_present = np.zeros((nz, nx))
 p_past = np.zeros((nz, nx))
 p_future = np.zeros((nz, nx))
 
-
 c0 = 2  # [m/s]
 
-# MELHORAR A PARTE ABAIXO:
-
 c = np.zeros((nz * nx))
-c[:] = c0
-c[:] += dc
-c = c.reshape(nx,nz)
+c = c.reshape(nx, nz)
 c = c.T
-# c[28 * p, :] = 2.000016747910872
-# c[29 * p, :] = 2.0048261353405694
-# c[30 * p, :] = 2.158098836842796
-# c[31 * p, :] = 2.3894003915357027
-# c[32 * p, :] = 1.6105996084642973
-# c[33 * p, :] = 1.841901163157204
-# c[34 * p, :] = 1.9951738646594308
-# c[35 * p, :] = 1.9999832520891276
-#
-# c[42 * p, :] = 2.0000167479108724
-# c[43 * p, :] = 2.0048261353405694
-# c[44 * p, :] = 2.158098836842796
-# c[45 * p, :] = 2.3894003915357027
-# c[46 * p, :] = 1.6105996084642973
-# c[47 * p, :] = 1.841901163157204
-# c[48 * p, :] = 1.9951738646594308
-# c[49 * p, :] = 1.9999832520891276
+c[:] = c0
+c[:] += dC
+
 
 
 # shots
@@ -135,10 +122,8 @@ pos_rec_z = zpos * np.ones_like(nr)  # matriz posição do receptor
 source_x = (nx // 2)
 source_z = zpos  # posição da fonte
 
-# pos_rec = pos_rec.astype(int)
-
 act = np.zeros((nt, nx))  # matriz das aquisições no tempo
-
+# actp = source
 
 for nt in range(nt):
 
@@ -157,14 +142,21 @@ for nt in range(nt):
     p_future[:, -1] = 0
 
     act[nt, :] = p_future[pos_rec_z, pos_rec_x]
+    act[nt, :] *= (-1)
 
-    plt.imsave(f"images/plot_{nt}.png", p_future, cmap='gray')
+    # actp[nt] = p_future[source_z,source_x]
 
-create_gif_and_video(nt)
+#     plt.imsave(f"images/plot_{nt}.png", p_future, cmap='gray')
+#
+# create_gif_and_video(nt)
 
 # plot shot
 plt.figure()
 plt.imshow(act, origin='upper', aspect='auto')
 plt.colorbar()
+plt.figure()
+plt.plot(actp)
 plt.show()
-print('done')
+
+# Para colocar breakpoint
+pass
